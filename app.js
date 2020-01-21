150ec2c7aab2e0f9952d8e53a00a6b0393076b03
Paul Walko <paul@walko.org>
2020-01-20 22:35:05 -0500

    start frontend

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+/* Import Routes */
+const indexRouter = require('./routes/index');
+
+/* Start app */
+const app = express();
+
+/* Configure view engine */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+/* Configure Routes */
+app.user('/', indexRouter);
+
+/* Error handling TODO */
+//// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  next(createError(404));
+//});
+//
+//// error handler
+//app.use(function(err, req, res, next) {
+//  // set locals, only providing error in development
+//  res.locals.message = err.message;
+//  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//  // render the error page
+//  res.status(err.status || 500);
+//  res.render('error');
+//});
+//
+//module.exports = app;
